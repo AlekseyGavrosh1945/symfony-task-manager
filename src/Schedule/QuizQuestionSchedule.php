462c4afb1465fb744f3d6f660a4638ba2c78ac1c
AlekseyGavrosh1945 <ganibal1953@mail.ru>
2026-09-22 08:26:14 +0300

    Stateful schedule with missed-run handling; drop empty recipe stub

--- src/Schedule/QuizQuestionSchedule.php
+++ src/Schedule/QuizQuestionSchedule.php
@@ -7,19 +7,30 @@ use Symfony\Component\Scheduler\Attribute\AsSchedule;
 use Symfony\Component\Scheduler\RecurringMessage;
 use Symfony\Component\Scheduler\Schedule;
 use Symfony\Component\Scheduler\ScheduleProviderInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 /**
  * Twice a day, at 12:00 and 19:00 (container timezone, Europe/Minsk).
+ *
+ * The schedule is stateful: if the worker was down at the scheduled time,
+ * only the last missed run is executed after restart.
  */
 #[AsSchedule('quiz')]
 final class QuizQuestionSchedule implements ScheduleProviderInterface
 {
+    public function __construct(
+        private readonly CacheInterface $cache,
+    ) {
+    }
+
     public function getSchedule(): Schedule
     {
         return (new Schedule())
             ->add(
                 RecurringMessage::cron('0 12 * * *', new SendQuizQuestion()),
                 RecurringMessage::cron('0 19 * * *', new SendQuizQuestion()),
-            );
+            )
+            ->stateful($this->cache)      // ensure missed tasks are executed
+            ->processOnlyLastMissedRun(true); // but only the last missed run
     }
 }
